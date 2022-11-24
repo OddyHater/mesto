688a77d75b6831c5078e282e203e606497e835eb
Roman Grishin <roman.grishin.me@gmail.com>
2022-11-24 22:33:28 +0600

    refactor: js edit function

--- script/script.js
+++ script/script.js
@@ -1,35 +1,42 @@
-let editButton = document.querySelector('.profile__edit-button'),
-    popUp = document.querySelector('.pop-up__wrapper'),
-    closeButton = popUp.querySelector('.pop-up__close-button'),
-    saveButton = popUp.querySelector('.pop-up__submit'),
-    profileEditName = popUp.querySelector('.pop-up__input_type_name'),
-    profileEditDescription = popUp.querySelector('.pop-up__input_type_description'),
-    profileName = document.querySelector('.profile__name'),
-    profileDescription = document.querySelector('.profile__description');
+const editButton = document.querySelector('.profile__edit-button'),
+      popUp = document.querySelector('.pop-up__wrapper'),
+      closeButton = popUp.querySelector('.pop-up__close-button'),
+      saveButton = popUp.querySelector('.pop-up__submit'),
+      profileEditName = popUp.querySelector('.pop-up__input_type_name'), // input "Имя" в pop-up
+      profileEditDescription = popUp.querySelector('.pop-up__input_type_description'), // input "О себе" в pop-up
+      profileName = document.querySelector('.profile__name'),
+      profileDescription = document.querySelector('.profile__description');
 
 function changeVisability() {
-  popUp.classList.toggle('visible');
+  popUp.classList.toggle('visible'); // вешаем/снимаем класс на попап
 }
 
 editButton.addEventListener('click', changeVisability);
 closeButton.addEventListener('click', changeVisability);
 
 function formSubmitHandler (evt) {
-  let profileEditName = popUp.querySelector('.pop-up__input_type_name'),
-      profileEditDescription = popUp.querySelector('.pop-up__input_type_description'),
-      profileName = document.querySelector('.profile__name'),
-      profileDescription = document.querySelector('.profile__description');
+  let nameFromPage =  profileName.textContent,
+      descriptionFromPage = profileDescription.textContent,
+      nameFromForm = profileEditName.value,
+      descriptionFromForm = profileEditDescription.value;
 
   evt.preventDefault(); 
 
-  profileName.textContent = profileEditName.value;
-  profileDescription.textContent = profileEditDescription.value;
-
-  changeVisability();
+  if(nameFromForm === nameFromPage && descriptionFromForm === descriptionFromPage) {  
+    /* Если не внесено никаких изменений, то просто закрываем попап, 
+    иначе меняем содержимое на странице.*/
+    changeVisability();
+  } else {
+    profileName.textContent = nameFromForm;
+    profileDescription.textContent = descriptionFromForm;
+    changeVisability();
+  } 
+   
 }
 
-  profileEditName.value = profileName.textContent;
-  profileEditDescription.value = profileDescription.textContent;
+profileEditName.value = profileName.textContent;  
+profileEditDescription.value = profileDescription.textContent;
+//контент со страницы отобразится в форме до редактирования.
 
 saveButton.addEventListener('click', formSubmitHandler); 
 
