5020e4361f2fe6c71a1eafe87ca150ee2df5614f
Roman Grishin <roman.grishin.me@gmail.com>
2022-11-25 15:05:16 +0600

    fix: fixes after first review. html,css,js

---
+++ script/index.js
@@ -0,0 +1,34 @@
+const editButton = document.querySelector('.profile__edit-button'),
+      popUp = document.querySelector('.popup'),
+      popUpForm = popUp.querySelector('.popup__form'),
+      closeButton = popUp.querySelector('.popup__close-button'),
+      profileEditName = popUp.querySelector('.popup__input_type_name'),
+      profileEditDescription = popUp.querySelector('.popup__input_type_description'),
+      profileName = document.querySelector('.profile__name'),
+      profileDescription = document.querySelector('.profile__description');
+
+function popUpOpen() {
+  profileEditName.value = profileName.textContent;
+  profileEditDescription.value = profileDescription.textContent;
+
+  popUp.classList.toggle('popup_opened');
+}
+
+function popUpClose() {
+  popUp.classList.toggle('popup_opened');
+}
+
+editButton.addEventListener('click', popUpOpen);
+closeButton.addEventListener('click', popUpClose);
+
+function formSubmitHandler (evt) {  
+  evt.preventDefault(); 
+
+  profileName.textContent = profileEditName.value;
+  profileDescription.textContent = profileEditDescription.value;
+  
+  popUpClose();
+}
+
+popUpForm.addEventListener('submit', formSubmitHandler); 
+
