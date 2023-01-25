0a7f9bcc4e9f67eb571536bffa67df7f848ad0de
Roman Grishin <roman.grishin.work@gmail.com>
2023-01-25 13:43:44 +0600

    codestyle

--- script/Card.js
+++ script/Card.js
@@ -2,73 +2,73 @@ import { initialCards } from "./cards.js";
 import { popUpImage, popUpImagePicture, popUpImageCaption } from './index.js';
 
 class Card {
-    constructor(data, templateSelector) {
-        this._name = data.name;
-        this._image = data.link;
-        this._templateSelector = templateSelector;
-    }
+  constructor(data, templateSelector) {
+    this._name = data.name;
+    this._image = data.link;
+    this._templateSelector = templateSelector;
+  };
 
-    _getTemplate() {
-        const cardTemplate = document
-            .querySelector(`${this._templateSelector}`)
-            .content.cloneNode(true);
+  _getTemplate() {
+    const cardTemplate = document
+      .querySelector(`${this._templateSelector}`)
+      .content.cloneNode(true);
+    return cardTemplate;
+  };
 
-        return cardTemplate;
-    }
+  _addLikeListener(template) {
+    this._likeButton = template.querySelector('.card__like');
 
-    _addLikeListener(template) {
-        this._likeButton = template.querySelector('.card__like');
+    this._likeButton.addEventListener('click', () => {
+      this._likeButton.classList.toggle('like_active');
+    });
+  };
 
-        this._likeButton.addEventListener('click', () => {
-            this._likeButton.classList.toggle('like_active');
-        });
-    }
+  _addTrashListener(template) {
+    this._trashButton = template.querySelector('.card__trash-button');        
+    this._cardElement = this._trashButton.closest('.card');
 
-    _addTrashListener(template) {
-        this._trashButton = template.querySelector('.card__trash-button');        
-        this._cardElement = this._trashButton.closest('.card');
+    this._trashButton.addEventListener('click', () => {
+      this._cardElement.remove();
+    });
+  };
 
-        this._trashButton.addEventListener('click', () => {
-            this._cardElement.remove();
-        });
-    }
+  _addOpenImagePopupListener(template) {
+    this._image = template.querySelector('.card__image');
 
-    _addOpenImagePopupListener(template) {
-        this._image = template.querySelector('.card__image');
-
-        this._image.addEventListener('click', () => {
-            popUpImagePicture.src = this._image;
-            popUpImagePicture.alt = this._name;
-            popUpImageCaption.textContent = this._name;
-            popUpImage.classList.add('popup_opened');
-        });
+    this._image.addEventListener('click', () => {
+      popUpImagePicture.src = this._image;
+      popUpImagePicture.alt = this._name;
+      popUpImageCaption.textContent = this._name;
+      popUpImage.classList.add('popup_opened');
+    });
 
         console.log(popUpImagePicture.src);
-    }
+  };
 
-    generateCard() {
-        this._template = this._getTemplate().querySelector('.card');
-        this._cardTitle = this._template.querySelector('.card__name');
-        this._cardImage = this._template.querySelector('.card__image');
+  generateCard() {
+    this._template = this._getTemplate().querySelector('.card');
+    this._cardTitle = this._template.querySelector('.card__name');
+    this._cardImage = this._template.querySelector('.card__image');
 
-        this._addLikeListener(this._template);
-        this._addTrashListener(this._template);
-        this._addOpenImagePopupListener(this._template, item);
+    this._addLikeListener(this._template);
+    this._addTrashListener(this._template);
+    this._addOpenImagePopupListener(this._template, item);
 
-        this._cardTitle.textContent = this._name;
-        this._cardImage.alt = this._name;
-        this._cardImage.src = this._image;
+    this._cardTitle.textContent = this._name;
+    this._cardImage.alt = this._name;
+    this._cardImage.src = this._image;
         
-        return this._template;
-    };
+    return this._template;
+  };
 
     
 }
 
 
 initialCards.forEach((item) => {
-    const card = new Card(item, '.template');
-    const preparedCard = card.generateCard();
+  const card = new Card(item, '.template');
+  const preparedCard = card.generateCard();
 
-    document.querySelector('.cards__list').append(preparedCard);
+  document.querySelector('.cards__list').append(preparedCard);
+  
 });
